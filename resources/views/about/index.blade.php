9e5ca744c2791146240a0fa0cf7b60779aaebcbb
rijkerd <richardaggrey7@gmail.com>
2021-05-31 09:15:29 +0300

    feat: :building_construction: implement base layout for about page

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+
+@section('title','iPF Softwares | A Full-Fledged Software Development House')
+
+@section('description','We are a passionate team of software engineers, innovators & designers focused on developing digital solutions that drive human behavior and engagement by using custom website design, development & strategies and mobile solutions, with the aim of helping businesses in Africa stand out in the digital world.')
+
+@section('styles')
+{{-- <link rel="stylesheet" href="{{asset('css/home.css')}}" /> --}}
+@endsection
+
+@section('content')
+@include('about.banner')
+@include('about.culture')
+@include('about.values')
+@endsection
+
+@section('scripts')
+
+@endsection
