db1aba3eecf3b75a22993661c813e359298194c3
rijkerd <richardaggrey7@gmail.com>
2021-05-31 09:16:57 +0300

    feat: implement our values section

---
+++ resources/views/about/values.blade.php
@@ -0,0 +1,31 @@
+<section id="values" class="py-10 h-96">
+    <div class="container">
+        <h1 class="text-4xl font-bold mb-5">Our Values</h1>
+        <div class="grid grid-cols-3">
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Honesty</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Motivation</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Hardwork</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Solution</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Debate, Then Commit</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+            <div class="flex flex-col w-80 py-5">
+                <h2 class="text-2xl text-left font-medium mb-3">Flawless</h2>
+                <p class="text-lg">But I must explain to you how all mistaken idea of denouncing pleasure and praising pain was</p>
+            </div>
+        </div>
+    </div>
+</section>
