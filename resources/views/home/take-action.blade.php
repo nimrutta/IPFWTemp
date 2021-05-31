12cf05c46e700d3e40f7ad30900544eba98cf299
nimrutta <nimrutta@gmail.com>
2021-05-31 10:52:36 +0300

    Complete CTA section

---
+++ resources/views/home/take-action.blade.php
@@ -0,0 +1,16 @@
+<section class="flex flex-col" style="margin-top: 0; background-color: #448FE9; height: 24.375rem">
+    <div class="pt-0 flex-1 container flex items-center justify-center h-96">
+        <div class="flex-1 justify-center" style="color: #ffffff; text-align: center">
+            <p class="mb-0text-5xl font-normal leading-relaxed" style="font-size: 3.125rem; width: 100%; text-align: center">
+                Make a smart investment
+            </p>
+            <p class="mb-11 text-4xl font-normal leading-relaxed" style="font-size: 2.375rem; width: 100%; text-align: center; margin-bottom: 2.5rem">
+                Start with our free cost calculator            </p>
+            <button class="" style="background-color: #114582">
+                <a href="#" class="btn capitalize">
+                    Get Started
+                </a>
+            </button>
+        </div>
+    </div>
+</section>
