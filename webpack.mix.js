bae99f1f08ba9993c9c64d5c845f535f281b3486
rijkerd <richardaggrey7@gmail.com>
2021-06-06 00:20:48 +0300

    chore: remove slider bundling

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,8 @@ const mix = require("laravel-mix");
 
 mix.postCss("resources/css/app.css", "public/css", [
     require("@tailwindcss/jit"),
-]).js("resources/js/Slider.js", "public/js");
+]);
+// .js("resources/js/Slider.js", "public/js");
 // mix.js('resources/js/app.js', 'public/js')
 //     .postCss("resources/css/app.css", "public/css").options({
 //       postCss: [
