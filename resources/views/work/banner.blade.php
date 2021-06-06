2121235cc809fc273d488a73256a514b22988a00
rijkerd <richardaggrey7@gmail.com>
2021-06-06 12:22:15 +0300

    feat: implement banner section for workpage

---
+++ resources/views/work/banner.blade.php
@@ -0,0 +1,4 @@
+<section class="container py-48 text-center">
+    <h1 class="text-6xl font-bold">Impactful Projects</h1>
+    <p class="px-24 pt-4 text-2xl">We are focused on building and innovating digital solutions for impact-oriented businesses and initiatives that link people with social and economic opportunities in Africa.</p>
+</section>
