dd90fab711ed7386ce360a7b79aed8ebbd9be7d5
rijkerd <richardaggrey7@gmail.com>
2021-06-07 11:34:24 +0300

    refactor: :lipstick: minor changes on review

--- resources/views/work/banner.blade.php
+++ resources/views/work/banner.blade.php
@@ -1,4 +1,4 @@
 <section class="container py-48 text-center">
-    <h1 class="text-6xl font-bold">Impactful Projects</h1>
+    <h1 style="color: #08213E;" class="text-6xl font-bold">Impactful Projects</h1>
     <p class="px-24 pt-4 text-2xl">We are focused on building and innovating digital solutions for impact-oriented businesses and initiatives that link people with social and economic opportunities in Africa.</p>
 </section>
--- resources/views/work/projects.blade.php
+++ resources/views/work/projects.blade.php
@@ -1,10 +1,14 @@
-<section class="p-8 container grid grid-cols-2 text-gray-50 gap-y-12 text-center">
-    <div class="px-4 bg-black w-9/12 h-9/12 text-gray-50 space-y-4 rounded-md flex flex-col items-center mx-auto">
-        <h1 class="block text-sm font-semibold pt-4">SDG GOAL 1 : NO POVERTY</h1>
-        <p class="block text-base">Supporting women and informal savings Groups with Care International</p>
-        <img class="w-full h-full" src="{{asset('img/sdgs.png')}}" alt="SDG Africa">
+<section class="container grid grid-cols-2 text-gray-50 gap-12 text-center">
+    <div class="h-screen">
+        <div class="px-4 h-2/3 bg-black text-gray-50 rounded-md flex flex-col justify-between items-center">
+            <div class="mb-12">
+                <h1 class="block text-sm font-semibold pt-4">SDG GOAL 1 : NO POVERTY</h1>
+                <p class="block text-base">Supporting women and informal savings Groups with Care International</p>
+            </div>
+            <img class="object-cover w-full" src="{{asset('img/sdgs.png')}}" alt="SDG Africa">
+        </div>
     </div>
-    <div style="background-color: #107E68;" class="w-9/12 h-9/12 space-y-4 px-4 rounded-md flex flex-col items-center mx-auto">
+    <div style="background-color: #107E68;" class="space-y-4 px-4 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 8 : DECENT WORK AND ECONOMIC GROWTH</span>
         <span class="block text-base">Revolutionising talent management in Africa, an incredible partnership with Niajiri Limited</span>
         <div>
