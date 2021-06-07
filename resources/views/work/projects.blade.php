22239bd52f596ca9938a9b8c5a118ba683bcaab3
rijkerd <richardaggrey7@gmail.com>
2021-06-07 10:17:25 +0300

    refactor: change css

--- resources/views/work/projects.blade.php
+++ resources/views/work/projects.blade.php
@@ -1,38 +1,38 @@
-<section class="py-8 container grid grid-cols-2 text-gray-50 gap-y-16">
-    <div class="bg-black w-10/12 h-10/12 text-gray-50 mx-auto space-y-4 rounded-md flex flex-1 flex-col items-center">
+<section class="p-8 container grid grid-cols-2 text-gray-50 gap-y-12 text-center">
+    <div class="px-4 bg-black w-9/12 h-9/12 text-gray-50 space-y-4 rounded-md flex flex-col items-center mx-auto">
         <h1 class="block text-sm font-semibold pt-4">SDG GOAL 1 : NO POVERTY</h1>
         <p class="block text-base">Supporting women and informal savings Groups with Care International</p>
-        <img class="object w-full h-full" src="{{asset('img/sdgs.png')}}" alt="SDG Africa">
+        <img class="w-full h-full" src="{{asset('img/sdgs.png')}}" alt="SDG Africa">
     </div>
-    <div style="background-color: #107E68;" class="w-10/12 h-10/12 mx-auto space-y-4 rounded-md flex flex-1 flex-col items-center">
+    <div style="background-color: #107E68;" class="w-9/12 h-9/12 space-y-4 px-4 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 8 : DECENT WORK AND ECONOMIC GROWTH</span>
         <span class="block text-base">Revolutionising talent management in Africa, an incredible partnership with Niajiri Limited</span>
         <div>
             <img src="{{asset('img/NIAJIRI-01.png')}}" alt="Niajiri">
         </div>
     </div>
-    <div style="background-color: #162A44;" class="w-10/12 h-10/12 mx-auto space-y-4 rounded-md flex flex-1 flex-col items-center">
+    <div style="background-color: #162A44;" class="w-9/12 h-9/12 px-4 space-y-4 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 17 : PARTNERSHIP FOR THE GOALS</span>
         <span class="block text-base text-center">A digital media campaign and photography on promoting the sustainable development goals with Sahara Ventures</span>
         <div>
             <img src="{{asset('img/INNOVATEpng-01.png')}}" alt="Innovate">
         </div>
     </div>
-    <div style="background-color: #E95569;" class="w-10/12 h-10/12 mx-auto space-y-4 bg-blue-400 rounded-md flex flex-1 flex-col items-center">
+    <div style="background-color: #E95569;" class="w-9/12 h-9/12 px-4 space-y-4 bg-blue-400 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 1 : NO POVERTY</span>
         <span class="block text-base text-center">Supporting women and informal savings Groups with Care International</span>
         <div>
             <img src="{{asset('img/riseup-01.png')}}" alt="SDG Africa">
         </div>
     </div>
-    <div style="background-color: #107E68;" class="w-10/12 h-10/12 mx-auto space-y-4 rounded-md flex flex-1 flex-col items-center">
+    <div style="background-color: #107E68;" class="w-9/12 h-9/12 px-4 space-y-4 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 8 : DECENT WORK AND ECONOMIC GROWTH</span>
         <span class="block text-base">Revolutionising talent management in Africa, an incredible partnership with Niajiri Limited</span>
         <div>
             <img src="{{asset('img/NIAJIRI-01.png')}}" alt="Niajiri">
         </div>
     </div>
-    <div style="background-color: #162A44;" class="w-10/12 h-10/12 mx-auto space-y-4 rounded-md flex flex-1 flex-col items-center">
+    <div style="background-color: #162A44;" class="w-9/12 h-9/12 px-4 space-y-4 rounded-md flex flex-col items-center mx-auto">
         <span class="block pt-4 text-sm font-semibold">SDG GOAL 17 : PARTNERSHIP FOR THE GOALS</span>
         <span class="block text-base text-center">A digital media campaign and photography on promoting the sustainable development goals with Sahara Ventures</span>
         <div>
